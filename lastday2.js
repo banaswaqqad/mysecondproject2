8b8dd7bfa95cd005dd44a609b63f7cccc5001f57
banaswaqqad <Banawaqqad7@gmail.com>
2021-02-24 21:56:50 +0200

    finallllly i did it by my self , using function

---
+++ lastday2.js
@@ -0,0 +1,16 @@
+
+var noImgess=parseInt( prompt("enter number of img"));
+
+
+
+for (var i=0;i<noImgess;i++)
+{
+
+function getthenoImgs(noImgess)
+{   
+    var imgss=noImgess;
+  document.write ('<img src="https://i.pinimg.com/originals/45/9c/90/459c9027f9f826354a1fac6ca9b5d714.jpg">');
+}
+
+getthenoImgs(noImgess);
+}
